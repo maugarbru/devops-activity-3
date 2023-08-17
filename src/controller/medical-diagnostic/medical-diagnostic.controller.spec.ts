b5f9912a087c524e7366fe3075e2ded9809d2f92
Sebastian Vargas <svargas9801@gmail.com>
2023-08-17 10:36:43 -0500

    add unit test

--- src/controller/medical-diagnostic/medical-diagnostic.controller.spec.ts
+++ src/controller/medical-diagnostic/medical-diagnostic.controller.spec.ts
@@ -4,6 +4,7 @@ import { MedicalDiagnosticService } from 'src/services/medical-diagnostic.servic
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { MedicalDiagnostic } from 'src/entities/medical-diagnostic.entity';
 
+const users = [];
 describe('MedicalDiagnosticController', () => {
   let controller: MedicalDiagnosticController;
   let service: MedicalDiagnosticService;
@@ -16,7 +17,7 @@ describe('MedicalDiagnosticController', () => {
         {
           provide: getRepositoryToken(MedicalDiagnostic),
           useValue: {
-            findAll: jest.fn(),
+            findAll: jest.fn(() => users),
             create: jest.fn(() => []),
             delete: jest.fn(() => []),
             update: jest.fn(() => []),
@@ -33,8 +34,22 @@ describe('MedicalDiagnosticController', () => {
     );
   });
 
-  it('should return all medical diagnostics', async () => {
-    jest.spyOn(service, 'findAll').mockImplementation(async () => []);
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should return and empty array if there are not registers', async () => {
+    jest.spyOn(service, 'findAll').mockImplementation(async () => users);
     expect(await service.findAll()).toStrictEqual([]);
   });
+
+  it('should return the list with medical diagnostic', async () => {
+    jest.spyOn(service, 'create').mockImplementation(async (body) => {
+      users.push(body);
+      return body;
+    });
+    jest.spyOn(service, 'findAll').mockImplementation(async () => users);
+    service.create({} as any);
+    expect((await service.findAll()).length).toBeGreaterThan(5);
+  })
 });
